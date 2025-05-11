2bfc6c36d69b949ea6e3c910a2e1b12f319eb4e2
1wuyi2 <649714272@qq.com>
2025-05-11 14:24:04 +0800

    更新 version2

--- csgame version2/floor.h
+++ csgame version2/floor.h
@@ -1,25 +1,3 @@
-// #ifndef FLOOR_H
-// #define FLOOR_H
-
-// #include <QGraphicsRectItem>
-// #include <QPainter>
-
-// class Floor : public QGraphicsRectItem {
-// public:
-//     explicit Floor(qreal ypos, QGraphicsItem *parent = nullptr);//构造函数
-//     qreal gety() const;//获取地板最上面的坐标
-
-// // protected:
-// //     void paint(QPainter *painter,
-// //                const QStyleOptionGraphicsItem *option,
-// //                QWidget *widget) override;
-
-// private:
-// static constexpr qreal width = 200.0;   // 地板宽度
-// static constexpr qreal thickness = 30.0; // 地板厚度
-// };
-
-// #endif // FLOOR_H
 #ifndef FLOOR_H
 #define FLOOR_H
 
